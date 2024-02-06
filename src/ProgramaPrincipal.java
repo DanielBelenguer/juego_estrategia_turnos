6ec706abeb08fbdc00d2a3ac1ba21beb510c4c78
Daniel <danielbelenguerfernandez@gmail.com>
2024-02-06 23:35:30 +0100

    Creacion de ejercitos funcional

--- src/ProgramaPrincipal.java
+++ src/ProgramaPrincipal.java
@@ -4,96 +4,50 @@ import java.lang.*;
 import java.util.Scanner;
 import java.util.Arrays;
 public class ProgramaPrincipal {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
-
         //Declaracion de variables
-        int costeTotal=0,posicion=0,opt;
-        //--------------------------------
-
-        // Creacion de ejercito Azul
         Personaje [] ejerAzul = new Personaje[1];
-        while (costeTotal < 50) {
-            System.out.println("Selecciona personaje");
-            opt = lector.nextInt();
-            switch (opt) {
-                case 1:
-                    ejerAzul[posicion] = new Soldado();
-                    costeTotal += ejerAzul[posicion].getCoste();
-                    posicion++;
-                    break;
-                case 2:
-                    ejerAzul[posicion] = new Caballero();
-                    costeTotal += ejerAzul[posicion].getCoste();
-                    posicion++;
-                    break;
-                case 3:
-                    ejerAzul[posicion] = new Arquero();
-                    costeTotal += ejerAzul[posicion].getCoste();
-                    posicion++;
-                    break;
-                case 4:
-                    ejerAzul[posicion] = new Lancero();
-                    costeTotal += ejerAzul[posicion].getCoste();
-                    posicion++;
-                    break;
-                default:
-                    System.out.println("Introduce opcion valida.");
-            }
-            // if para aumentar la array ejercito azul
-            if (costeTotal < 50) {
-                ejerAzul = Arrays.copyOf(ejerAzul, ejerAzul.length + 1);
-                ejerAzul[ejerAzul.length - 1] = null;
+        Personaje [] ejerRojo = new Personaje[1];
+        // creamos el tablero
+        Tablero mapa = new Tablero();
+        // Creamos ejercito Azul
+        int sumacoste=0,contador=0;
+        while (sumacoste < 50) {
+            System.out.println("""
+                    Tienes 50 puntos para gastar
+                    1-Soldado 10 puntos, 2-Caballero 25, 3-Arquero 15,4-Lancero 5
+                    Seleccione personajes: """);
+            ejerAzul[contador] = mapa.crearEjercito(lector.nextInt());
+            sumacoste+=ejerAzul[contador].getCoste();
+            contador++;
+            if (sumacoste < 50) {
+                //Crea una copia con longitud + 1 y la asigna a la variable array
+                ejerAzul = Arrays.copyOf(ejerAzul, ejerAzul.length+1);
+                ejerAzul[ejerAzul.length-1] = null;
             }
         }
-        System.out.println("Ahora a crear el ejercito Rojo");
+        // Creamos ejercito Rojo
+        sumacoste=0;contador=0;
+        while (sumacoste < 50) {
+            System.out.println("Elije un personaje: ");
+            ejerRojo[contador] = mapa.crearEjercito(lector.nextInt());
+            sumacoste+=ejerRojo[contador].getCoste();
+            contador++;
 
-            // Creacion ejercito Rojo
-            Personaje[] ejerRojo = new Personaje[1];
-            // Reiniciamos las variables
-            costeTotal = 0;
-            posicion = 0;
-            //--------------------------
-            while (costeTotal < 50){
-                System.out.println("Selecciona personaje");
-                opt = lector.nextInt();
-                switch (opt) {
-                    case 1:
-                        ejerRojo[posicion] = new Soldado();
-                        costeTotal += ejerRojo[posicion].getCoste();
-                        posicion++;
-                        break;
-                    case 2:
-                        ejerRojo[posicion] = new Caballero();
-                        costeTotal += ejerRojo[posicion].getCoste();
-                        posicion++;
-                        break;
-                    case 3:
-                        ejerRojo[posicion] = new Arquero();
-                        costeTotal += ejerRojo[posicion].getCoste();
-                        posicion++;
-                        break;
-                   case 4:
-                       ejerRojo[posicion] = new Lancero();
-                       costeTotal += ejerRojo[posicion].getCoste();
-                       posicion++;
-                       break;
-                   default:
-                       System.out.println("Introduce opcion valida.");
-               }
-               // if para aumentar la array ejercito rojo
-               if (costeTotal < 50) {
-                   ejerRojo = Arrays.copyOf(ejerRojo, ejerRojo.length + 1);
-                   ejerRojo[ejerRojo.length - 1] = null;
-               }
-           }
-           // Continua programa.
-            for (Personaje perso : ejerAzul){
-                System.out.println(perso);
+            if (sumacoste < 50){
+                //Crea una copia con longitud + 1 y la asigna a la variable array
+                ejerRojo = Arrays.copyOf(ejerRojo,ejerRojo.length+1);
+                ejerRojo[ejerRojo.length-1] = null;
             }
-            for (Personaje perso : ejerRojo){
-                System.out.println(perso);
-            }
+        }
+        for (Personaje per: ejerAzul){
+            System.out.println(per);
+        }
+        for (Personaje per :ejerRojo){
+            System.out.println(per);
+        }
 
-       }
    }
+
+}
--- src/tablero/Tablero.java
+++ src/tablero/Tablero.java
@@ -2,8 +2,13 @@ package tablero;
 
 import personajes.*;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Tablero {
     Personaje[][] tablero = new Personaje[8][8];
+
+
     public void mostrarTablero (){
 
         System.out.print("       ");  //ocupamos el espacio en blanco de la izquierda
@@ -25,6 +30,30 @@ public class Tablero {
             }
             System.out.println();
         }
+    }
+    public Personaje crearEjercito(int opt){
+            System.out.println("Tienes un total de 50 puntos por gastar.\n" +
+                    "1 - Soldado 10 puntos\n2 - Caballero  25 puntos\n3 - Arquero 15 puntos\n4 - Lancero 5 puntos" +
+                    "\nElige personaje para crear ");
+
+            switch (opt){
+                case 1:
+                    Personaje sol = new Soldado();
+                    return sol;
+                case 2:
+                    Personaje cab = new Caballero();
+                    return cab;
+
+                case 3:
+                    Personaje arq = new Arquero();
+                    return arq;
+                case 4:
+                    Personaje lan = new Lancero();
+                    return lan;
+                default:
+                    System.out.println("Introduce opción válida.");
+            }
+            return null;
     }
 
 }
